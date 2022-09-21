3407c76239e509afc1ad424fcfd45261605ea172
OddShapedRock <86754685+oddshapedrock@users.noreply.github.com>
2022-09-20 21:56:31 -0500

    work on maze gen model

--- maze_generation.py
+++ maze_generation.py
@@ -1,6 +1,6 @@
 """# TODO: """
 import random
-
+import timeit
 __all__ = [
 "generate",
 "generate_tiny"
@@ -11,6 +11,7 @@ class GenerateMaze():
     def __init__(self):
         self.maze_size = [0, 0]
         self.grid_map = []
+        self.snake = {"direction": 0, "location": [0, 0], "number": 1}
 
     def generate(self, maze_size):
         '''Generates a maze of [width, height] size as a python dictionary'''
@@ -23,35 +24,60 @@ class GenerateMaze():
             self.grid_map.append([])
             #creates grid columns
             for y_pos in range(maze_size[1]):
-                self.grid_map[x_pos].append([{
+                self.grid_map[x_pos].append({
                     "walls": [True, True, True, True], #[top, bottom, left, right]
                     "isMaze": False,
                     "visited": False,
                     "location": [x_pos, y_pos] #[top, bottom]
-                    }])
+                    })
         print(self.grid_map)
 
-        snake = {"direction": 0, "location": [0, 0]}
-
-        snake["direction"] = self.new_direction([True, True, True, True])
-        print(snake)
+        self.new_direction([0,1,2,3])
+        moves = self.get_possible_moves()
+        print(moves)
 
     def new_direction(self, possible_moves):
         """# TODO: """
-        amount = -1
-        for direction in possible_moves:
-            if direction:
-                amount += 1
+        random_number = random.randint(0, len(possible_moves) - 1)
+        self.snake["direction"] = possible_moves[random_number]
 
-        if not amount == -1:
-            random_number = random.randint(0, amount)
-            index = 0
-            while index < len(possible_moves):
-                if possible_moves[index] and not random_number:
-                    return index
-                index += 1
-                random_number -= 1
-        return -1
+    def get_pos(self, x_pos, y_pos):
+        """# TODO: """
+        return self.grid_map[x_pos][y_pos]
+
+    def get_possible_moves(self):
+        """# TODO: """
+        moves_list = []
+        snake_x = self.snake["location"][0]
+        snake_y = self.snake["location"][1]
+        if snake_y + 1 < self.maze_size[0]:
+            dir_up = self.get_pos(snake_x, snake_y +1)
+            if not dir_up["visited"] or dir_up.get["isMaze"]:
+                moves_list.append(0)
+        if snake_y -1 >= 0:
+            dir_down = self.get_pos(snake_x, snake_y -1)
+            if not dir_down["visited"] or dir_down["isMaze"]:
+                moves_list.append(1)
+        if snake_x -1 >= 0:
+            dir_left = self.get_pos(snake_x -1, snake_y)
+            if not dir_left["visited"] or dir_left["isMaze"]:
+                moves_list.append(2)
+        if snake_x + 1 < self.maze_size[0]:
+            dir_right = self.get_pos(snake_x +1, snake_y)
+            if not dir_right["visited"] or dir_right["isMaze"]:
+                moves_list.append(3)
+
+        return moves_list
+
+    def make_path(self, starting_location):
+        """# TODO: """
+        saved_locations = [starting_location]
+        snake = self.snake
+        snake["location"] = starting_location
+        while True:
+            self.new_direction(self.get_possible_moves())
+            break
+        return
 
     def generate_tiny(self, maze_size):
         """# TODO: """
--- test.py
+++ test.py
@@ -1,6 +1,24 @@
 """thing"""
-
+import random
+from timeit import default_timer as timer
 import maze_generation
-maze_generation.generate([2, 2])
+# import maze_generation
+# maze_generation.generate([2, 2])
+#most effiecient
+def t3(possible_moves):
+    def ifTrue(x):
+        return x[1]
+    filter(ifTrue, possible_moves)
+    if len(possible_moves):
+        random_number = random.randint(0, len(possible_moves) -1)
+        return possible_moves[random_number][0]
+    return - 1
 
-print()
+
+def t5(possible_moves):
+    random_number = random.randint(0, len(possible_moves) - 1)
+    return possible_moves[random_number]
+
+
+
+maze_generation.generate([5, 5])
